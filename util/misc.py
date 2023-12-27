193c1c24fe8461d8f2afd8101a060aff267825b3
kostaskon7 <57294205+kostaskon7@users.noreply.github.com>
2023-12-27 20:53:50 +0200

    Update misc.py
    Updated misc torch import to latest version for google colab execution

--- util/misc.py
+++ util/misc.py
@@ -7,7 +7,9 @@ from pathlib import Path
 
 import torch
 import torch.distributed as dist
-from torch._six import inf
+#from torch._six import inf
+# Depends on torch version if late version then the following
+from torch import inf
 
 
 class SmoothedValue(object):
