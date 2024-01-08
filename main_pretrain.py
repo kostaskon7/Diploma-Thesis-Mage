0e7cd9cdae76233395479cceba6f1f5bc32e46a9
kostaskon7 <kostaskon7@gmail.com>
2024-01-08 15:05:16 +0200

    Adding COCO support 3

--- main_pretrain.py
+++ main_pretrain.py
@@ -159,7 +159,7 @@ def main(args):
     train_loader = torch.utils.data.DataLoader(train_dataset, sampler=train_sampler, shuffle=True, drop_last=True, batch_size=args.batch_size, num_workers= 4)
 
     val_dataset = COCO2017(root=args.data_path, split='val', image_size=256, mask_size=256)
-    val_loader = torch.utils.data.DataLoader(val_dataset, sampler=val_sampler, shuffle=False, drop_last=False, batch_size=args.eval_batch_size, num_workers= 4)
+    val_loader = torch.utils.data.DataLoader(val_dataset, sampler=val_sampler, shuffle=False, drop_last=False, batch_size=args.batch_size, num_workers= 4)
 
     # define the model
     vqgan_ckpt_path = args.vqgan_ckpt_path
