f9d6e77520c8c57f09bc743038d283d73b8e2711
kostaskon7 <kostaskon7@gmail.com>
2024-01-09 13:34:47 +0200

    Adding COCO support 3

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -110,7 +110,7 @@ def train_one_epoch(model: torch.nn.Module, data_loader: Iterable, optimizer: to
         print('log_dir: {}'.format(log_writer.log_dir))
 
     for data_iter_step, batch_data in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-        samples = batch_data[0].to(device, non_blocking=True)  # Use only the first tensor (images)
+        samples = batch_data.to(device, non_blocking=True)  # Use only the first tensor (images)
         print(samples.shape)
         # LR Scheduler Adjustment (if necessary)
         if data_iter_step % accum_iter == 0:
