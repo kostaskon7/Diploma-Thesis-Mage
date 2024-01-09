46c302f9afc3f6b8691d8f2732c8a7156dbc72b3
kostaskon7 <kostaskon7@gmail.com>
2024-01-09 13:22:41 +0200

    Adding COCO support 3

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -8,72 +8,127 @@ import util.misc as misc
 import util.lr_sched as lr_sched
 
 
-def train_one_epoch(model: torch.nn.Module,
-                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
-                    device: torch.device, epoch: int, loss_scaler,
-                    log_writer=None,
-                    args=None):
+# def train_one_epoch(model: torch.nn.Module,
+#                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
+#                     device: torch.device, epoch: int, loss_scaler,
+#                     log_writer=None,
+#                     args=None):
+#     model.train(True)
+#     metric_logger = misc.MetricLogger(delimiter="  ")
+#     metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+#     header = 'Epoch: [{}]'.format(epoch)
+#     print_freq = 20
+
+#     accum_iter = args.accum_iter
+
+#     optimizer.zero_grad()
+
+#     if log_writer is not None:
+#         print('log_dir: {}'.format(log_writer.log_dir))
+
+#     # Add this code before your training loop to inspect the data_loader output
+#     # for batch in data_loader:
+#     #     print(f"Number of items in batch: {len(batch)}")
+#     #     print([type(item) for item in batch])
+#     #     break  # Only inspect the first batch
+#     for batch in data_loader:
+#         print(f"Number of items in batch: {len(batch)}")
+#         print([type(item) for item in batch])
+#         break  # Inspect just the first batch
+
+#     for batch in data_loader:
+#         for i, item in enumerate(batch):
+#             if isinstance(item, torch.Tensor):
+#                 print(f"Item {i}: shape {item.shape}")
+#             else:
+#                 print(f"Item {i}: type {type(item)}")
+#                 if(i==1):
+#                     print(item)
+#         break  # Inspect just the first batch
+
+
+
+
+
+#     for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+#     # for data_iter_step, (samples, mask_instance, mask_class, mask_ignore) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+
+#         # we use a per iteration (instead of per epoch) lr scheduler
+#         if data_iter_step % accum_iter == 0:
+#             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+
+#         samples = samples.to(device, non_blocking=True)
+
+#         with torch.cuda.amp.autocast():
+#             loss, _, _ = model(samples)
+
+#         loss_value = loss.item()
+
+#         if not math.isfinite(loss_value):
+#             print("Loss is {}, stopping training".format(loss_value))
+#             sys.exit(1)
+
+#         loss /= accum_iter
+#         loss_scaler(loss, optimizer, clip_grad=args.grad_clip, parameters=model.parameters(),
+#                     update_grad=(data_iter_step + 1) % accum_iter == 0)
+#         if (data_iter_step + 1) % accum_iter == 0:
+#             optimizer.zero_grad()
+
+#         torch.cuda.synchronize()
+
+#         metric_logger.update(loss=loss_value)
+
+#         lr = optimizer.param_groups[0]["lr"]
+#         metric_logger.update(lr=lr)
+
+#         loss_value_reduce = misc.all_reduce_mean(loss_value)
+#         if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
+#             """ We use epoch_1000x as the x-axis in tensorboard.
+#             This calibrates different curves when batch size changes.
+#             """
+#             epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
+#             log_writer.add_scalar('train_loss', loss_value_reduce, epoch_1000x)
+#             log_writer.add_scalar('lr', lr, epoch_1000x)
+
+
+#     # gather the stats from all processes
+#     metric_logger.synchronize_between_processes()
+#     print("Averaged stats:", metric_logger)
+#     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+def train_one_epoch(model: torch.nn.Module, data_loader: Iterable, optimizer: torch.optim.Optimizer, device: torch.device, epoch: int, loss_scaler, log_writer=None, args=None):
     model.train(True)
     metric_logger = misc.MetricLogger(delimiter="  ")
     metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
     header = 'Epoch: [{}]'.format(epoch)
     print_freq = 20
-
     accum_iter = args.accum_iter
 
     optimizer.zero_grad()
-
     if log_writer is not None:
         print('log_dir: {}'.format(log_writer.log_dir))
 
-    # Add this code before your training loop to inspect the data_loader output
-    # for batch in data_loader:
-    #     print(f"Number of items in batch: {len(batch)}")
-    #     print([type(item) for item in batch])
-    #     break  # Only inspect the first batch
-    for batch in data_loader:
-        print(f"Number of items in batch: {len(batch)}")
-        print([type(item) for item in batch])
-        break  # Inspect just the first batch
+    for data_iter_step, batch_data in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+        samples = batch_data[0].to(device, non_blocking=True)  # Use only the first tensor (images)
 
-    for batch in data_loader:
-        for i, item in enumerate(batch):
-            if isinstance(item, torch.Tensor):
-                print(f"Item {i}: shape {item.shape}")
-            else:
-                print(f"Item {i}: type {type(item)}")
-        break  # Inspect just the first batch
-
-
-
-
-
-    for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-    # for data_iter_step, (samples, mask_instance, mask_class, mask_ignore) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-
-        # we use a per iteration (instead of per epoch) lr scheduler
+        # LR Scheduler Adjustment (if necessary)
         if data_iter_step % accum_iter == 0:
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
-
-        samples = samples.to(device, non_blocking=True)
 
         with torch.cuda.amp.autocast():
             loss, _, _ = model(samples)
 
         loss_value = loss.item()
-
         if not math.isfinite(loss_value):
             print("Loss is {}, stopping training".format(loss_value))
             sys.exit(1)
 
         loss /= accum_iter
-        loss_scaler(loss, optimizer, clip_grad=args.grad_clip, parameters=model.parameters(),
-                    update_grad=(data_iter_step + 1) % accum_iter == 0)
+        loss_scaler(loss, optimizer, clip_grad=args.grad_clip, parameters=model.parameters(), update_grad=(data_iter_step + 1) % accum_iter == 0)
         if (data_iter_step + 1) % accum_iter == 0:
             optimizer.zero_grad()
 
         torch.cuda.synchronize()
-
         metric_logger.update(loss=loss_value)
 
         lr = optimizer.param_groups[0]["lr"]
@@ -81,15 +136,10 @@ def train_one_epoch(model: torch.nn.Module,
 
         loss_value_reduce = misc.all_reduce_mean(loss_value)
         if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
-            """ We use epoch_1000x as the x-axis in tensorboard.
-            This calibrates different curves when batch size changes.
-            """
             epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
             log_writer.add_scalar('train_loss', loss_value_reduce, epoch_1000x)
             log_writer.add_scalar('lr', lr, epoch_1000x)
 
-
-    # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
--- main_pretrain.py
+++ main_pretrain.py
@@ -125,36 +125,36 @@ def main(args):
             transforms.ToTensor()])
 
 
-    dataset_train = datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform_train)
-    #print(dataset_train)
+    # dataset_train = datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform_train)
+    # #print(dataset_train)
 
-    if True:  # args.distributed:
-        num_tasks = misc.get_world_size()
-        global_rank = misc.get_rank()
-        sampler_train = torch.utils.data.DistributedSampler(
-            dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True
-        )
-        print("Sampler_train = %s" % str(sampler_train))
-    else:
-        sampler_train = torch.utils.data.RandomSampler(dataset_train)
+    # if True:  # args.distributed:
+    #     num_tasks = misc.get_world_size()
+    #     global_rank = misc.get_rank()
+    #     sampler_train = torch.utils.data.DistributedSampler(
+    #         dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True
+    #     )
+    #     print("Sampler_train = %s" % str(sampler_train))
+    # else:
+    #     sampler_train = torch.utils.data.RandomSampler(dataset_train)
 
-    if global_rank == 0 and args.log_dir is not None:
-        os.makedirs(args.log_dir, exist_ok=True)
-        log_writer = SummaryWriter(log_dir=args.log_dir)
-    else:
-        log_writer = None
+    # if global_rank == 0 and args.log_dir is not None:
+    #     os.makedirs(args.log_dir, exist_ok=True)
+    #     log_writer = SummaryWriter(log_dir=args.log_dir)
+    # else:
+    #     log_writer = None
 
-    data_loader_train = torch.utils.data.DataLoader(
-        dataset_train, sampler=sampler_train,
-        batch_size=args.batch_size,
-        num_workers=args.num_workers,
-        pin_memory=args.pin_mem,
-        drop_last=True,
-    )
+    # data_loader_train = torch.utils.data.DataLoader(
+    #     dataset_train, sampler=sampler_train,
+    #     batch_size=args.batch_size,
+    #     num_workers=args.num_workers,
+    #     pin_memory=args.pin_mem,
+    #     drop_last=True,
+    # )
 
-    # log_writer = SummaryWriter(log_dir=args.log_dir)
-    # train_sampler = None
-    # val_sampler = None
+    log_writer = SummaryWriter(log_dir=args.log_dir)
+    train_sampler = None
+    val_sampler = None
 
 
     # def custom_collate_fn(batch):
@@ -169,24 +169,24 @@ def main(args):
 
 
 
-    #train_dataset = COCO2017(root=args.data_path, split='train', image_size=256, mask_size=256)
-    for index in range(len(sampler_train)):
-        # Fetch the item
-        item = sampler_train[index]
+    train_dataset = COCO2017(root=args.data_path, split='train', image_size=256, mask_size=256)
+    # for index in range(len(sampler_train)):
+    #     # Fetch the item
+    #     item = sampler_train[index]
 
-        # Print the index and the shape of the item
-        print(f"Index: {index}")
+    #     # Print the index and the shape of the item
+    #     print(f"Index: {index}")
 
-        if isinstance(item, tuple):
-            for i, tensor in enumerate(item):
-                print(f"  Shape of tensor {i}: {tensor.shape}")
-        else:
-            print(f"  Shape of the item: {item.shape}")    
+    #     if isinstance(item, tuple):
+    #         for i, tensor in enumerate(item):
+    #             print(f"  Shape of tensor {i}: {tensor.shape}")
+    #     else:
+    #         print(f"  Shape of the item: {item.shape}")    
     
-    # train_loader = torch.utils.data.DataLoader(train_dataset, sampler=train_sampler, shuffle=True, drop_last=True, batch_size=args.batch_size, pin_memory=True,num_workers= 4)#,collate_fn=custom_collate_fn)
+    train_loader = torch.utils.data.DataLoader(train_dataset, sampler=train_sampler, shuffle=True, drop_last=True, batch_size=args.batch_size, pin_memory=True,num_workers= 4)#,collate_fn=custom_collate_fn)
 
-    # val_dataset = COCO2017(root=args.data_path, split='val', image_size=256, mask_size=256)
-    # val_loader = torch.utils.data.DataLoader(val_dataset, sampler=val_sampler, shuffle=False, drop_last=False, batch_size=args.batch_size, pin_memory=True,num_workers= 4)#,collate_fn=custom_collate_fn)
+    val_dataset = COCO2017(root=args.data_path, split='val', image_size=256, mask_size=256)
+    val_loader = torch.utils.data.DataLoader(val_dataset, sampler=val_sampler, shuffle=False, drop_last=False, batch_size=args.batch_size, pin_memory=True,num_workers= 4)#,collate_fn=custom_collate_fn)
 
     # define the model
     vqgan_ckpt_path = args.vqgan_ckpt_path
