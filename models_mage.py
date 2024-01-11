9e229e3ebc2938fb5952bc98742de1d05fd212de
kostaskon7 <kostaskon7@gmail.com>
2024-01-11 21:15:57 +0200

    Adding COCO support 3

--- models_mage.py
+++ models_mage.py
@@ -378,6 +378,7 @@ class MaskedGenerativeEncoderViT(nn.Module):
         print(bsz)
         print(seq_len)
         print(mask.shape)
+        print(logits.shape)
         # logits and mask are with seq_len+1 but gt_indices is with seq_len
         loss = self.criterion(logits[:, 1:, :self.codebook_size].reshape(bsz*seq_len, -1), gt_indices.reshape(bsz*seq_len))#DEN EIMAI SIGOUROS GIA TO +1 H +7
         print(loss.shape)
