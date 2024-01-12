a059cc03ea56c3e79512c0fc56733cf05f48c830
kostaskon7 <kostaskon7@gmail.com>
2024-01-12 20:49:43 +0200

    Adding COCO support 3

--- main_pretrain.py
+++ main_pretrain.py
@@ -319,6 +319,11 @@ def main(args):
                 print(batch_size)
                 default_slots_attns = default_slots_attns[:, 1:, :]
                 dec_slots_attns=dec_slots_attns[:, 1:, :]
+
+                print(default_slots_attns.shape)
+                print(dec_slots_attns.shape)
+                print(batch_size)
+
                 default_slots_attns = default_slots_attns.transpose(-1, -2).reshape(batch_size, 7, 16, 16)
                 dec_slots_attns = dec_slots_attns.transpose(-1, -2).reshape(batch_size, 7, 16, 16)
                 # default_slots_attns=default_slots_attns.unsqueeze(3)
