b462d4a09eae43f62ea6f99b4b2187e5aa6b41ea
kostaskon7 <kostaskon7@gmail.com>
2024-01-18 00:20:27 +0200

    Adding COCO support 3

--- main_pretrain.py
+++ main_pretrain.py
@@ -258,14 +258,14 @@ def main(args):
     print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
     for epoch in range(args.start_epoch, args.epochs):
-        # if args.distributed:
-        #     train_loader.sampler.set_epoch(epoch)
-        # train_stats = train_one_epoch(
-        #     model, train_loader,
-        #     optimizer, device, epoch, loss_scaler,
-        #     log_writer=log_writer,
-        #     args=args
-        # )
+        if args.distributed:
+            train_loader.sampler.set_epoch(epoch)
+        train_stats = train_one_epoch(
+            model, train_loader,
+            optimizer, device, epoch, loss_scaler,
+            log_writer=log_writer,
+            args=args
+        )
         # if args.output_dir and (epoch % 40 == 0 or epoch + 1 == args.epochs):
         #     misc.save_model(
         #         args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
