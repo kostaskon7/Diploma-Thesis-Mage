ceac105efa96c9ea824ba4aa7233de3910bcb921
kostaskon7 <kostaskon7@gmail.com>
2024-01-21 14:43:51 +0200

    Another run change

--- main_pretrain.py
+++ main_pretrain.py
@@ -256,15 +256,15 @@ def main(args):
 
     print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
-    for epoch in range(args.start_epoch, 30):
-        # if args.distributed:
-        #     train_loader.sampler.set_epoch(epoch)
-        # train_stats = train_one_epoch(
-        #     model, train_loader,
-        #     optimizer, device, epoch, loss_scaler,
-        #     log_writer=log_writer,
-        #     args=args
-        # )
+    for epoch in range(args.start_epoch, 9):
+        if args.distributed:
+            train_loader.sampler.set_epoch(epoch)
+        train_stats = train_one_epoch(
+            model, train_loader,
+            optimizer, device, epoch, loss_scaler,
+            log_writer=log_writer,
+            args=args
+        )
         # if args.output_dir and (epoch % 40 == 0 or epoch + 1 == args.epochs):
         #     misc.save_model(
         #         args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
@@ -325,7 +325,7 @@ def main(args):
                 # print(batch_size)
                 #################
                 ##Recon
-                if(epoch>3):
+                if(epoch>9):
                     codebook_emb_dim=256
                     logits = logits[:, 8:, :model.codebook_size]
                     probabilities = torch.nn.functional.softmax(logits, dim=-1)
