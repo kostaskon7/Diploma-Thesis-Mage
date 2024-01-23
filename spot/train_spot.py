92570d00c4681edb0dae7a58e568532373ef4e25
kostaskon7 <kostaskon7@gmail.com>
2024-01-24 00:16:25 +0200

    Another run change

--- spot/train_spot.py
+++ spot/train_spot.py
@@ -218,7 +218,7 @@ def train(args):
     
     visualize_per_epoch = int(args.epochs*args.eval_viz_percent)
     
-    for epoch in range(start_epoch, 16):
+    for epoch in range(start_epoch, 24):
     
         model.train()
     
