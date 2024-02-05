5991da5396142cfff50ecb6d5331fc928884c4fd
kostaskon7 <kostaskon7@gmail.com>
2024-02-05 14:24:02 +0200

    Another run change

--- main_pretrain.py
+++ main_pretrain.py
@@ -175,18 +175,6 @@ def main(args):
     val_sampler = None
 
 
-    # def custom_collate_fn(batch):
-    #     # Stack the images (assuming they are at index 0 in the tuples and have 3 color channels)
-    #     images = torch.stack([item[0] for item in batch])  # This should now have the shape [32, 3, 256, 256]
-
-    #     # Concatenate or stack the second items (assuming they are labels or similar)
-    #     # Adjust this line according to what the second item actually represents
-    #     labels = torch.cat([item[1].unsqueeze(0) for item in batch], dim=0)  # This should now have the shape [32]
-
-    #     return images, labels
-
-
-
     train_dataset = COCO2017(root=args.data_path, split='train', image_size=256, mask_size=256)
 
     
