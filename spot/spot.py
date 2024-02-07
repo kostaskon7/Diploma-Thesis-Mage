929e01bc274909c0375bde8d1cd70f31719e052d
kostaskon7 <kostaskon7@gmail.com>
2024-02-07 16:08:13 +0200

    Another run change

--- spot/spot.py
+++ spot/spot.py
@@ -356,7 +356,8 @@ class SPOT(nn.Module):
         # slots_attns shape: [B, N, num_slots]
 
         # Apply the decoder.
-        dec_recon, dec_slots_attns = self.forward_decoder(slots, emb_target[:,1:,:])
+        # dec_recon, dec_slots_attns = self.forward_decoder(slots, emb_target[:,1:,:])
+        dec_recon, dec_slots_attns = self.forward_decoder_generation(slots, emb_target[:,1:,:])
 
         # Mean-Square-Error loss
         H_enc, W_enc = int(math.sqrt(emb_target.shape[1])), int(math.sqrt(emb_target.shape[1]))
