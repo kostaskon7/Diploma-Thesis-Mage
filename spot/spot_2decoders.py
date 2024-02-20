b37755fa6c813d01d5dfa1647116b55a5ea73588
kostaskon7 <kostaskon7@gmail.com>
2024-02-20 14:11:57 +0200

    Another run change

--- spot/spot_2decoders.py
+++ spot/spot_2decoders.py
@@ -5,6 +5,10 @@ from mlp import MlpDecoder
 import torch
 import random
 import math
+from functools import partial
+from timm.models.vision_transformer import PatchEmbed, DropPath, Mlp
+
+
 
 
 class SPOT(nn.Module):
@@ -64,6 +68,20 @@ class SPOT(nn.Module):
 
         # --------------------------------------------------------------------------
         # MAGE decoder specifics
+
+        embed_dim=1024
+        decoder_embed_dim=512
+        num_patches = self.patch_embed.num_patches
+        decoder_num_heads=16
+        patch_size=16
+        mlp_ratio=4
+        norm_layer=partial(nn.LayerNorm, eps=1e-6)
+        dropout_rate = 0.1
+        decoder_depth=8
+        in_chans=3
+        vocab_size = self.encoder.codebook_size + 1000 + 1
+
+
         self.decoder_embed = nn.Linear(embed_dim, decoder_embed_dim, bias=True)
 
         self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
@@ -471,3 +489,79 @@ class SPOT(nn.Module):
 
 
         return loss_out, slots_attns, dec_slots_attns, slots, dec_recon, attn_logits
+
+
+
+
+class MlmLayer(nn.Module):
+
+    def __init__(self, feat_emb_dim, word_emb_dim, vocab_size):
+        super().__init__()
+        self.fc = nn.Linear(feat_emb_dim, word_emb_dim)
+        self.gelu = nn.GELU()
+        self.ln = nn.LayerNorm(word_emb_dim)
+        self.bias = nn.Parameter(torch.zeros(1, 1, vocab_size))
+
+    def forward(self, x, word_embeddings):
+        mlm_hidden = self.fc(x)
+        mlm_hidden = self.gelu(mlm_hidden)
+        mlm_hidden = self.ln(mlm_hidden)
+        word_embeddings = word_embeddings.transpose(0, 1)
+        logits = torch.matmul(mlm_hidden, word_embeddings)
+        logits = logits + self.bias
+        return logits
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+
+        with torch.cuda.amp.autocast(enabled=False):
+            attn = (q.float() @ k.float().transpose(-2, -1)) * self.scale
+
+        attn = attn - torch.max(attn, dim=-1, keepdim=True)[0]
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x, attn
+
+class Block(nn.Module):
+
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x, return_attention=False):
+        if return_attention:
+            _, attn = self.attn(self.norm1(x))
+            return attn
+        else:
+            y, _ = self.attn(self.norm1(x))
+            x = x + self.drop_path(y)
+            x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
