fb940e1d18587d9aaa34220197d21e935528f228
kostaskon7 <kostaskon7@gmail.com>
2024-02-21 23:20:54 +0200

    Another run change

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -118,7 +118,8 @@ def train_one_epoch(model: torch.nn.Module, data_loader: Iterable, optimizer: to
         with torch.cuda.amp.autocast():
             if args.use_decs:
                 loss, _, _,_,_,_ = model(samples)
-                loss, _ = loss
+                loss_mage, loss_mage_spot = loss
+                loss=(0.5*loss_mage)+(0.5*loss_mage_spot)
             else:
                 loss, _, _,_,_,_ = model(samples)
 
