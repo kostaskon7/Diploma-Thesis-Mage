0cdf15956584705f8ed41fee4e5c8628bd62d22f
kostaskon7 <kostaskon7@gmail.com>
2024-02-21 19:01:39 +0200

    Another run change

--- models_mage.py
+++ models_mage.py
@@ -201,8 +201,8 @@ class MaskedGenerativeEncoderViT(nn.Module):
         self.slot_attention = SlotAttentionEncoder(
             num_iterations=3,  # specify the number of iterations
             num_slots=7,       # specify the number of slots
-            input_channels=embed_dim,  # since it should match the output of your encoder
-            slot_size=768,       # specify the slot size
+            input_channels=768,  # since it should match the output of your encoder ###embed_dim
+            slot_size=256,       # specify the slot size
             mlp_hidden_size=1024, # specify the MLP hidden size
             pos_channels=4,    # specify the positional channels size
             truncate='none', # or other options as per your requirement
