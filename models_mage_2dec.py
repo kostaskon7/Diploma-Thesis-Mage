01d284def7960135ec3d58f55c817ff30661c5ab
kostaskon7 <kostaskon7@gmail.com>
2024-02-21 18:10:01 +0200

    Another run change

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -574,7 +574,7 @@ class MaskedGenerativeEncoderViT(nn.Module):
 
             # dec_input has the same shape as emb_target, which is [B, N, D]
             dec_input = self.input_proj(dec_input)
-    
+            print(slots.shape)
             # Apply the decoder
             dec_input_slots = self.slot_proj(slots) # shape: [B, num_slots, D]
 
