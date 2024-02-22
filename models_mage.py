018d34371cb2966d9668c2e52d1d49feadc7a3a8
kostaskon7 <kostaskon7@gmail.com>
2024-02-22 14:22:36 +0200

    Another run change

--- models_mage.py
+++ models_mage.py
@@ -200,11 +200,21 @@ class MaskedGenerativeEncoderViT(nn.Module):
             for i in range(depth)])
         self.norm = norm_layer(embed_dim)
         # --------------------------------------------------------------------------
+        # self.slot_attention = SlotAttentionEncoder(
+        #     num_iterations=3,  # specify the number of iterations
+        #     num_slots=7,       # specify the number of slots
+        #     input_channels=768,  # since it should match the output of your encoder ###embed_dim
+        #     slot_size=256,       # specify the slot size
+        #     mlp_hidden_size=1024, # specify the MLP hidden size
+        #     pos_channels=4,    # specify the positional channels size
+        #     truncate='none', # or other options as per your requirement
+        #     init_method='shared_gaussian'  # or 'shared_gaussian'
+        # )
         self.slot_attention = SlotAttentionEncoder(
             num_iterations=3,  # specify the number of iterations
             num_slots=7,       # specify the number of slots
-            input_channels=768,  # since it should match the output of your encoder ###embed_dim
-            slot_size=256,       # specify the slot size
+            input_channels=256,  # since it should match the output of your encoder ###embed_dim
+            slot_size=768,       # specify the slot size
             mlp_hidden_size=1024, # specify the MLP hidden size
             pos_channels=4,    # specify the positional channels size
             truncate='none', # or other options as per your requirement
