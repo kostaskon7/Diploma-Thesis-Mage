b5e764bdc8e25865d1355e294b47095919269a0c
kostaskon7 <kostaskon7@gmail.com>
2024-02-25 13:47:07 +0200

    Another run change

--- models_mage.py
+++ models_mage.py
@@ -521,7 +521,7 @@ class MaskedGenerativeEncoderViT(nn.Module):
         # slots, attn, init_slots, attn_logits = self.slot_attention(latent[:,1:,:])
 
         slots, attn, _, _ = self.slot_attention(latent)
-        # slots=self.slot_proj(slots)
+        slots=self.slot_proj(slots)
         # print(latent.shape)
         # logits = self.forward_decoder(latent, token_drop_mask, token_all_mask)
         # logits,attn_dec = self.forward_decoder(latent,latent ,token_drop_mask, token_all_mask)
