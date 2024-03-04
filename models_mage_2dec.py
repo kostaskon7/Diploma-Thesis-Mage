7fb8ffd965033dca4d37731e5fb80139d2ccb6c7
kostaskon7 <kostaskon7@gmail.com>
2024-03-04 14:32:44 +0200

    Another run change

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -699,6 +699,8 @@ class MaskedGenerativeEncoderViT(nn.Module):
 
         loss_mage = self.forward_loss(gt_indices, logits, token_all_mask)
         loss_spot = ((latent[:,1:,:] - dec_recon) ** 2).sum()/(16*16*16*self.d_model)
+        torch.cuda.empty_cache()
+
 
 
         loss=(loss_mage,loss_spot)
