7b60db67f18328f256383cfdd3282b5d7e191542
kostaskon7 <kostaskon7@gmail.com>
2024-03-27 16:45:34 +0200

    Another run change

--- gen_img_cond.py
+++ gen_img_cond.py
@@ -149,14 +149,14 @@ def gen_image(model, image, bsz, seed, num_iter=12, choice_temperature=4.5,per_i
             # Reshape back to the original slots tensor shape
             slots_replaced = slots_replaced.view_as(slots)
         logits,_ = model.forward_decoder(x, slots_replaced, token_drop_mask, token_all_mask)
-        logits = logits[:, model.slot_attention.num_slots+1:, :codebook_size]
+        # logits = logits[:, model.slot_attention.num_slots+1:, :codebook_size]
 
 
 
         # decoder
         # logits,_ = model.forward_decoder(x, slots, token_drop_mask, token_all_mask)
         # logits = logits[:, model.slot_attention.num_slots+1:, :codebook_size]
-        # logits = logits[:, n_top_slots+1:, :codebook_size]
+        logits = logits[:, n_top_slots+1:, :codebook_size]
 
         # get token prediction
         sample_dist = torch.distributions.categorical.Categorical(logits=logits)
