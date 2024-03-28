939e7456d3597051f58373bf247d68bf8b2ec4f7
kostaskon7 <kostaskon7@gmail.com>
2024-03-28 18:51:23 +0200

    Another run change

--- gen_img_cond.py
+++ gen_img_cond.py
@@ -129,7 +129,7 @@ def gen_image(model, image, bsz, seed, num_iter=12, choice_temperature=4.5,per_i
         
         # Find top k slots
         if step==0:
-            n_top_slots = 1
+            n_top_slots = 6
             slots_summed_values = slots.sum(dim=2)
             _, top_slot_indices = slots_summed_values.topk(n_top_slots, dim=1)
             slots = torch.gather(slots, 1, top_slot_indices.unsqueeze(-1).expand(-1, -1, slots.size(2)))
