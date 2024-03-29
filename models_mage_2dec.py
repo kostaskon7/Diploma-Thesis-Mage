707194fa0f49214dd008e26753c7805a3077dd96
kostaskon7 <kostaskon7@gmail.com>
2024-03-29 14:17:45 +0200

    Another run change

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -869,9 +869,10 @@ def mage_vit_base_patch16(**kwargs):
 
     model.freeze_decoder()
 
-    for block in model.decoder_blocks:
-        for param in block.mage_cross_attn.parameters():
-            param.requires_grad = True
+    
+    # for block in model.decoder_blocks:
+    #     for param in block.mage_cross_attn.parameters():
+    #         param.requires_grad = True
 
 
 
