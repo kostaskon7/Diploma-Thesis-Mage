c1edb3ca912fbb625e84e9b9308a213b4720676f
kostaskon7 <kostaskon7@gmail.com>
2024-04-01 14:19:09 +0300

    Another run change

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -773,10 +773,10 @@ class MaskedGenerativeEncoderViT(nn.Module):
         return loss
 
     def forward(self, imgs):
-        with torch.no_grad():
-            latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
+        # with torch.no_grad():
+        latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
 
-            latent= self.forward_encoder(imgs)
+        latent= self.forward_encoder(imgs)
         #slots, attn, init_slots, attn_logits = self.slot_attention(latent[:,1:,:])
         latent=latent[:,1:,:]
 
