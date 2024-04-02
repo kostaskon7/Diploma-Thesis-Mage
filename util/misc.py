7cd6696b2adf614b73e14950c2e35f42ed40f35e
kostaskon7 <kostaskon7@gmail.com>
2024-04-02 17:14:01 +0300

    Another run change

--- util/misc.py
+++ util/misc.py
@@ -255,7 +255,6 @@ class NativeScalerWithGradNormCount:
             else:
                 self._scaler.unscale_(optimizer)
                 norm = get_grad_norm_(parameters)
-            breakpoint()
             self._scaler.step(optimizer)
             self._scaler.update()
         else:
