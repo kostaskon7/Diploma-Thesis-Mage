6e8bb986e5ec0a8a51767d0696e1df3dfd9fccc1
kostaskon7 <kostaskon7@gmail.com>
2024-04-08 20:36:11 +0300

    kmeans devhard slot creation

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -778,7 +778,7 @@ class MaskedGenerativeEncoderViT(nn.Module):
         latent= self.forward_encoder(imgs)
             
         latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
-        latent_mask=latent_mask.clone().detach()
+        # latent_mask=latent_mask.clone().detach()
         
         #slots, attn, init_slots, attn_logits = self.slot_attention(latent[:,1:,:])
         latent=latent[:,1:,:]
@@ -875,7 +875,7 @@ def mage_vit_base_patch16(**kwargs):
         decoder_embed_dim=768, decoder_depth=8, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
 
-    # model.freeze_encoder()
+    model.freeze_encoder()
 
     # model.freeze_decoder()
 
