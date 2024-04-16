088582fd8805f88e8f8d898457478a71cdbff681
kostaskon7 <kostaskon7@gmail.com>
2024-04-16 19:23:27 +0300

    run hard mask without norm

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -857,7 +857,7 @@ class MaskedGenerativeEncoderViT(nn.Module):
         attn=attn.clone().detach()
         attn_onehot = torch.nn.functional.one_hot(attn.argmax(2), num_classes=self.slot_attention.num_slots).to(latent.dtype)
         # To add normalization
-        attn_onehot = attn_onehot / torch.sum(attn_onehot+self.epsilon, dim=-2, keepdim=True)
+        # attn_onehot = attn_onehot / torch.sum(attn_onehot+self.epsilon, dim=-2, keepdim=True)
         slots_pool = torch.matmul(attn_onehot.transpose(-1, -2), latent)
         slots_pool=self.slot_proj2(slots_pool)
 
