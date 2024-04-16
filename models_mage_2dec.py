08ee584d7a9003c9c35f311bfb2f7cd243ab5029
kostaskon7 <kostaskon7@gmail.com>
2024-04-16 21:23:47 +0300

    run hard mask without norm change order 2

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -851,9 +851,27 @@ class MaskedGenerativeEncoderViT(nn.Module):
         latent=latent[:,1:,:]
 
         slots, attn, _, attn_logits = self.slot_attention(latent)
-        attn_logits = attn_logits.transpose(-1, -2).reshape(bsz, self.slot_attention.num_slots, H_enc, W_enc)
-        attn_transpose = attn.transpose(-1, -2).reshape(bsz, self.slot_attention.num_slots, H_enc, W_enc)
+
         
+        if self.training:
+
+            attn_logits = attn_logits.transpose(-1, -2).reshape(bsz, self.slot_attention.num_slots, H_enc, W_enc)
+            attn_transpose = attn.transpose(-1, -2).reshape(bsz, self.slot_attention.num_slots, H_enc, W_enc)   
+
+            attn_logits = F.interpolate(attn_logits, size=mask_crf.shape[-1], mode='bilinear')
+            attn_interpolate = F.interpolate(attn_transpose, size=mask_crf.shape[-1], mode='bilinear')
+            attn_onehot_crf = torch.nn.functional.one_hot(attn_interpolate.argmax(1), num_classes=self.slot_attention.num_slots).permute(0,3,1,2)
+            mask_crf_onehot = torch.nn.functional.one_hot(mask_crf, num_classes=self.slot_attention.num_slots).permute(0,3,1,2).to(dtype=torch.float16)
+
+            permutation_indices, _ = att_matching(attn_onehot_crf, mask_crf_onehot)
+            
+            attn_logits = torch.stack([x[permutation_indices[n]] for n, x in enumerate(attn_logits)], dim=0)
+
+
+            ce_loss = self.criterion_masks(attn_logits, mask_crf_onehot)
+        else:
+            ce_loss=0
+
         # Hard Mask pooling
         attn=attn.clone().detach()
         attn_onehot = torch.nn.functional.one_hot(attn.argmax(2), num_classes=self.slot_attention.num_slots).to(latent.dtype)
@@ -879,23 +897,7 @@ class MaskedGenerativeEncoderViT(nn.Module):
         loss_spot = ((latent - dec_recon) ** 2).sum()/(bsz*H_enc*W_enc*self.d_model)
 
         # Crf
-        if self.training:
 
-
-
-            attn_logits = F.interpolate(attn_logits, size=mask_crf.shape[-1], mode='bilinear')
-            attn_interpolate = F.interpolate(attn_transpose, size=mask_crf.shape[-1], mode='bilinear')
-            attn_onehot_crf = torch.nn.functional.one_hot(attn_interpolate.argmax(1), num_classes=self.slot_attention.num_slots).permute(0,3,1,2)
-            mask_crf_onehot = torch.nn.functional.one_hot(mask_crf, num_classes=self.slot_attention.num_slots).permute(0,3,1,2).to(dtype=torch.float16)
-
-            permutation_indices, _ = att_matching(attn_onehot_crf, mask_crf_onehot)
-            
-            attn_logits = torch.stack([x[permutation_indices[n]] for n, x in enumerate(attn_logits)], dim=0)
-
-
-            ce_loss = self.criterion_masks(attn_logits, mask_crf_onehot)
-        else:
-            ce_loss=0
 
 
 
