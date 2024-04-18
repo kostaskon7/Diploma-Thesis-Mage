9a2365fbe1361aadced57ebb8cf4b8fa30b78b9c
kostaskon7 <kostaskon7@gmail.com>
2024-04-18 12:02:55 +0300

    save al checks

--- main_pretrain_2dec.py
+++ main_pretrain_2dec.py
@@ -562,8 +562,8 @@ def main(args):
                 'model': model.state_dict(),
                 'optimizer': optimizer.state_dict()
             }
-            if (epoch % 10 == 0) or (epoch > args.epochs -5):
-                torch.save(checkpoint, os.path.join(args.output_dir, "checkpoint-%s.pth" % epoch))
+            # if (epoch % 10 == 0) or (epoch > args.epochs -5):
+            torch.save(checkpoint, os.path.join(args.output_dir, "checkpoint-%s.pth" % epoch))
 
             print('====> Best Loss = {:F} @ Epoch {}'.format(best_val_loss, best_epoch))
 
