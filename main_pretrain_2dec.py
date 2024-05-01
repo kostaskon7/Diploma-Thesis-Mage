bbaece13fbfb24b025ac5e990b8eecfe2f036b82
kostaskon7 <kostaskon7@gmail.com>
2024-05-01 20:56:12 +0300

    visualize teacher

--- main_pretrain_2dec.py
+++ main_pretrain_2dec.py
@@ -535,7 +535,8 @@ def main(args):
                 rgb_default_attns = image.unsqueeze(1) * default_attns + 1. - default_attns
                 rgb_dec_attns = image.unsqueeze(1) * dec_attns + 1. - dec_attns
     
-                vis_recon = visualize(image, true_mask_c, pred_dec_mask, rgb_dec_attns, pred_default_mask, rgb_default_attns, N=32)
+                # vis_recon = visualize(image, true_mask_c, pred_dec_mask, rgb_dec_attns, pred_default_mask, rgb_default_attns, N=32)
+                vis_recon = visualize(image, true_mask_c, pred_dec_mask, rgb_dec_attns, mask_crf_val, mask_crf_val, N=32)
                 grid = vutils.make_grid(vis_recon, nrow=2*args.num_slots + 4, pad_value=0.2)[:, 2:-2, 2:-2]#anti gia 7 num_slots
                 grid = F.interpolate(grid.unsqueeze(1), scale_factor=0.15, mode='bilinear').squeeze() # Lower resolution
                 log_writer.add_image('VAL_recon/epoch={:03}'.format(epoch), grid)
@@ -546,7 +547,8 @@ def main(args):
                 rgb_default_attns = image.unsqueeze(1) * default_attns + 1. - default_attns
                 rgb_dec_attns = image.unsqueeze(1) * mage_dec_attns + 1. - mage_dec_attns
     
-                vis_recon = visualize(image, true_mask_c, pred_mage_dec_mask, rgb_dec_attns, pred_default_mask, rgb_default_attns, N=32)
+                # vis_recon = visualize(image, true_mask_c, pred_mage_dec_mask, rgb_dec_attns, pred_default_mask, rgb_default_attns, N=32)
+                vis_recon = visualize(image, true_mask_c, pred_mage_dec_mask, rgb_dec_attns, mask_crf_val, mask_crf_val, N=32)
                 grid = vutils.make_grid(vis_recon, nrow=2*args.num_slots + 4, pad_value=0.2)[:, 2:-2, 2:-2]#anti gia 7 num_slots
                 grid = F.interpolate(grid.unsqueeze(1), scale_factor=0.15, mode='bilinear').squeeze() # Lower resolution
                 log_writer.add_image('VAL_recon_mage/epoch={:03}'.format(epoch), grid)
