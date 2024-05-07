3837688d88b9e0abfc88b25b60a8cd1b6a18506d
kostaskon7 <kostaskon7@gmail.com>
2024-05-07 22:59:48 +0300

    kmeans before linear layer change dir

--- k_means_slots.py
+++ k_means_slots.py
@@ -254,7 +254,7 @@ kmeans.fit(data_2d_np)
 
 
 
-directory = '/data/kmeans/hard_100_no_tol_max_iter_500/'
+directory = '/data/kmeans/hard_100_no_tol_max_iter_500_no_proj/'
 file_name = 'kmeans_model1024_100ep_hard.joblib'
 
 full_path = os.path.join(directory, file_name)
