4477344148d930f3f5bfe76513e478c32b16537c
kostaskon7 <kostaskon7@gmail.com>
2024-05-07 17:00:26 +0300

    kmeans ad hard mask pooling change tol to 0.0001 σλοτ αττ

--- k_means_slots.py
+++ k_means_slots.py
@@ -163,32 +163,32 @@ for batch, image in enumerate(tqdm(val_loader, desc="Processing images")):
         #slots, attn, init_slots, attn_logits = self.slot_attention(latent[:,1:,:])
         latent=latent[:,1:,:]
 
-        # slots, attn, _, _ = model.slot_attention(latent)
+        slots, attn, _, _ = model.slot_attention(latent)
 
         # slots=model.slot_proj2(slots)
 
-        slots, attn, attn_logits = model.masked_trans(latent,(16, 16))
+        # slots, attn, attn_logits = model.masked_trans(latent,(16, 16))
 
         
 
 
-        # Hard Mask pooling
-        attn=attn.clone().detach()
-        attn_onehot = torch.nn.functional.one_hot(attn.argmax(2), num_classes=model.slot_attention.num_slots).to(latent.dtype)
-        # To add normalization
-        # attn_onehot = attn_onehot / torch.sum(attn_onehot+self.epsilon, dim=-2, keepdim=True)
-        slots_pool = torch.matmul(attn_onehot.transpose(-1, -2), latent)
-        slots=model.slot_proj2(slots_pool)
-
+        # # Hard Mask pooling
         # attn=attn.clone().detach()
-        # Latent another transformation?
-        # attn_onehot = torch.nn.functional.one_hot(attn.argmax(2), num_classes=7).to(latent.dtype)
+        # attn_onehot = torch.nn.functional.one_hot(attn.argmax(2), num_classes=model.slot_attention.num_slots).to(latent.dtype)
+        # # To add normalization
+        # # attn_onehot = attn_onehot / torch.sum(attn_onehot+self.epsilon, dim=-2, keepdim=True)
         # slots_pool = torch.matmul(attn_onehot.transpose(-1, -2), latent)
+        # slots=model.slot_proj2(slots_pool)
+
+        attn=attn.clone().detach()
+        # Latent another transformation?
+        attn_onehot = torch.nn.functional.one_hot(attn.argmax(2), num_classes=7).to(latent.dtype)
+        slots_pool = torch.matmul(attn_onehot.transpose(-1, -2), latent)
 
 
         # slots_pool = torch.matmul(attn.transpose(-1, -2), latent)
 
-        # slots_pool=model.slot_proj2(slots_pool)
+        slots=model.slot_proj2(slots_pool)
         collected_outputs.append(slots)
     
 
