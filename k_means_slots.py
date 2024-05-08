a645e468b158cb79798acd54ef0f2b90717e3f72
kostaskon7 <kostaskon7@gmail.com>
2024-05-08 21:39:02 +0300

    check slots imidiately in decoder

--- k_means_slots.py
+++ k_means_slots.py
@@ -231,7 +231,7 @@ kmeans.fit(data_2d_np)
 
 
 
-directory = '/data/kmeans/hard_100_tol_1e-4-soft-no-proj/'
+directory = '/data/kmeans/hard_100_tol_1e-4-norm/'
 file_name = 'kmeans_model1024_100ep_hard.joblib'
 
 full_path = os.path.join(directory, file_name)
