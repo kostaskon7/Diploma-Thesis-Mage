93b3092090af82b4071ab49bfe0276b831df958f
kostaskon7 <kostaskon7@gmail.com>
2024-05-08 21:37:01 +0300

    check slots imidiately in decoder

--- k_means_slots.py
+++ k_means_slots.py
@@ -198,41 +198,10 @@ for batch, image in enumerate(tqdm(val_loader, desc="Processing images")):
         # breakpoint()
         # slots_pool = torch.matmul(attn.transpose(-1, -2), latent)
 
-        # slots=model.slot_proj2(slots)
+        slots=model.slot_proj2(slots)
         collected_outputs.append(slots)
     
 
-
-# all_slots = torch.cat(collected_outputs, dim=0)
-
-
-# all_slots_reshaped = all_slots.view(-1,7, 256)
-# # Now all_outputs is [total_images, 7, 256], directly ready for KMeans without additional reshaping
-# all_slots_reshaped=all_slots_reshaped.cuda()
-
-
-# total_elements = all_slots.numel()
-
-# Since the last dimension is fixed at 256, calculate the middle dimension dynamically
-# Note: 'total_elements // 256' gives the total size for the first two dimensions combined
-# The new shape's middle dimension is calculated by dividing the total size by the number of batches and then by 256
-# new_middle_dimension = (total_elements // 256) // all_slots.shape[0]
-
-# Reshape the combined tensor to the new shape
-# reshaped_slots = all_slots.reshape(all_slots.shape[0], new_middle_dimension, 256)
-
-
-
-# Make sure your KMeans supports GPU, and `all_outputs_reshape` is on the right device
-# kmeans_model = kmeans(num_classes=81, mode='euclidean', verbose=1, device=device)
-# labels = kmeans_model.fit(all_slots_reshaped)
-
-# cluster_ids_x, cluster_centers = kmeans(
-#     X=all_slots_reshaped, num_clusters=1024, distance='euclidean', device=device,tol=6e-3
-# )
-
-# model = KMeans(n_clusters=1024,tol=6e-3)
-# model = model.fit(reshaped_slots)
 
 
 
