c124c6baea2420be1d4fe298e3eada12dec4dc68
kostaskon7 <kostaskon7@gmail.com>
2024-05-09 11:49:23 +0300

    image gen debug kmeans models

--- k_means_slots.py
+++ k_means_slots.py
@@ -233,7 +233,7 @@ kmeans.fit(data_2d_np)
 
 
 
-directory = '/data/kmeans/hard_100_tol_1e-4-norm/'
+directory = '/data/kmeans/hard_100_tol_1e-4-check-models/'
 file_name = 'kmeans_model1024_100ep_hard.joblib'
 
 full_path = os.path.join(directory, file_name)
