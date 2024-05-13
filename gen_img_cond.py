e82612505dcdaaae6704ff1cbfe7a4dbb0900a54
kostaskon7 <kostaskon7@gmail.com>
2024-05-13 19:51:14 +0300

    check with normalization but not kmeans centroids

--- gen_img_cond.py
+++ gen_img_cond.py
@@ -165,11 +165,11 @@ def gen_image(model, image, bsz, seed, num_iter=12, choice_temperature=4.5,per_i
 
     breakpoint()
 
-    # Reshape back to the original slots shape
-    slots = centers.reshape(-1, slots_tensor.shape[1], 768)  # Use the original num_slots
-    # slots = centers.reshape(-1, 256, 7)  # Use the original num_slots
+    # # Reshape back to the original slots shape
+    # slots = centers.reshape(-1, slots_tensor.shape[1], 768)  # Use the original num_slots
+    # # slots = centers.reshape(-1, 256, 7)  # Use the original num_slots
 
-    slots = torch.tensor(slots).cuda()
+    # slots = torch.tensor(slots).cuda()
 
     # # Find the indices of the maximum values (most important features) from the soft attention
     # max_indices = torch.argmax(attn, dim=-1)
