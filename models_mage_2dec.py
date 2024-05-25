99214583ed3d2afd03d7c50ae012fb9e32b25958
kostaskon7 <kostaskon7@gmail.com>
2024-05-25 17:55:32 +0300

    change testing branch with classic model add losses

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -836,10 +836,10 @@ class MaskedGenerativeEncoderViT(nn.Module):
 
     def forward(self, imgs):
         
-        # with torch.no_grad():
+        with torch.no_grad():
             # latent,_,_,_= self.forward_encoder_copy(imgs)
-        latent= self.forward_encoder(imgs)
-        latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
+            latent= self.forward_encoder(imgs)
+            latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
         # latent_mask=latent_mask.clone().detach()
         
 
