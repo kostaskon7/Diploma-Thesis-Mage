8780e7ef1a90a1f812dfbb5053cec47236d1fad8
kostaskon7 <kostaskon7@gmail.com>
2024-05-28 22:04:22 +0300

    run without layer norm

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -836,7 +836,9 @@ class MaskedGenerativeEncoderViT(nn.Module):
     def forward(self, imgs,mask_crf):
         
         # with torch.no_grad():
-        latent,_,_,_= self.forward_encoder_copy(imgs)
+        # latent,_,_,_= self.forward_encoder_copy(imgs)
+        latent= self.forward_encoder(imgs)
+
         latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
         # latent_mask=latent_mask.clone().detach()
         
