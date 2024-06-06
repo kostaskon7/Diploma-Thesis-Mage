065ab3d54d396ab0794b7d4befff63a9670a1634
kostaskon7 <kostaskon7@gmail.com>
2024-06-06 18:09:26 +0300

    small change on model with slots and hard mask pool

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -838,9 +838,9 @@ class MaskedGenerativeEncoderViT(nn.Module):
 
     def forward(self, imgs,mask_crf):
         
-        with torch.no_grad():
-            latent= self.forward_encoder(imgs)
-            latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
+        # with torch.no_grad():
+        latent= self.forward_encoder(imgs)
+        latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
         # latent_mask=latent_mask.clone().detach()
         
         bsz, _ = gt_indices.size()
@@ -869,7 +869,7 @@ class MaskedGenerativeEncoderViT(nn.Module):
 
 
         # dec_recon, dec_slots_attns=self.forward_decoder_spot(slots, latent)
-        dec_recon, dec_slots_attns=self.forward_decoder_spot(slots_pool, latent)
+        dec_recon, dec_slots_attns=self.forward_decoder_spot(slots, latent)
 
         #[Batch,decoder264,2025]
 
