a3360cb34f1a9e1c2fd8fcf640d8795b5e577157
kostaskon7 <kostaskon7@gmail.com>
2024-06-11 18:36:04 +0300

    detach slot attn input

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -839,9 +839,13 @@ class MaskedGenerativeEncoderViT(nn.Module):
 
     def forward(self, imgs,mask_crf):
         
-        with torch.no_grad():
-            latent= self.forward_encoder(imgs)
+        # with torch.no_grad():
+        #     latent= self.forward_encoder(imgs)
+        # latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
+        # with torch.no_grad():
+        latent= self.forward_encoder(imgs)
         latent_mask, gt_indices, token_drop_mask, token_all_mask = self.forward_encoder_mask(imgs)
+        latent = latent.clone().detach()
         # latent_mask=latent_mask.clone().detach()
         
         bsz, _ = gt_indices.size()
