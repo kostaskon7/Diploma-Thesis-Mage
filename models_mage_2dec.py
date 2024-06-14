c75b1c6035655e2898b7d52ee62d4b17d3ed0028
kostaskon7 <kostaskon7@gmail.com>
2024-06-14 18:06:50 +0300

    stage 3 debug error on optimizer load

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -948,10 +948,10 @@ class MaskedGenerativeEncoderViT(nn.Module):
 
         # dec_recon, dec_slots_attns=self.forward_decoder_spot(slots, latent)
         #[Batch,decoder264,2025]
-            if self.apply_mask.item():
-                loss_slots = self.slot_loss(logits[:,:self.slot_attention.num_slots],cluster_assignments,uniform_mask)
-            else:
-                loss_slots = 0
+        if self.apply_mask.item():
+            loss_slots = self.slot_loss(logits[:,:self.slot_attention.num_slots],cluster_assignments,uniform_mask)
+        else:
+            loss_slots = 0
         
         print(loss_slots)
         print(cluster_assignments)
