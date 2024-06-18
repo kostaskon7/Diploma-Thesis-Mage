5042e8893b7d2fb5e6c8d1a93468018de372b94c
kostaskon7 <kostaskon7@gmail.com>
2024-06-18 21:23:10 +0300

    stage 3 debug error on optimizer load

--- models_mage_2dec.py
+++ models_mage_2dec.py
@@ -574,6 +574,7 @@ class MaskedGenerativeEncoderViT(nn.Module):
 
         # apply Transformer blocks
         x = input_embeddings_after_drop
+        breakpoint()
         for blk in self.blocks:
             x = blk(x)
         x = self.norm(x)
