2f4fa05cc000b6e23078ee66da75d5a17d9a53db
kostaskon7 <kostaskon7@gmail.com>
2024-07-04 16:37:29 +0300

    debug stage 3 loss

--- gen_img_cond.py
+++ gen_img_cond.py
@@ -205,7 +205,6 @@ def gen_image(model, image, bsz, seed, num_iter=12, choice_temperature=4.5,per_i
                     # Replace the slot with the most probable cluster center
                     if torch.argmax(probs[i, slot_index]).item() ==3:
                         print("Pobability of slot 3 is: "+str(torch.sort(probs[i, slot_index], descending=True).values[0].item())+"Selecting Next")
-                        breakpoint()
                         most_probable_center_idx = torch.sort(probs[i, slot_index], descending=True).indices[1]
 
                     slots[i, slot_index] = cluster_centers[most_probable_center_idx]
